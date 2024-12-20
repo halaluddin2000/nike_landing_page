c2ad3b3593c922f790f0c4692d1381d303ab5c96
halaluddin2000 <helaluddin7001@gmail.com>
2024-12-21 01:35:31 +0600

    app section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-function App() {
-  return (
-    <div>
-      <h2 className="text-2xl font-semibold bg-slate-500">Hello world</h2>
-    </div>
-  );
-}
+const App = () => (
+  <main className="relative">
+    nav
+    <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+    <section className="padding">Popular products</section>
+    <section className="padding">superQuality</section>
+    <section className="padding">service</section>
+    <section className="padding">SpecialOffer</section>
+    <section className="bg-pale-blue padding ">CustomerReviews</section>
+    <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+    <section className="bg-black padding-x padding-t pb-8">Footer</section>
+  </main>
+);
 
 export default App;
