4766bd650347c8b5fe4f0c2443e6465aed5eedca
halaluddin2000 <helaluddin7001@gmail.com>
2024-12-26 23:15:45 +0600

    sheo card added

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,8 @@
 import Button from "../constants/Button";
 import { arrowRight } from "../assets/icons";
-import { statistics } from "../constants";
+import { shoes, statistics } from "../constants";
 import { bigShoe1 } from "../assets/images";
+import ShoeCard from "../Components/ShoeCard";
 
 const Hero = () => {
   return (
@@ -45,6 +46,17 @@ const Hero = () => {
           height={500}
           className="object-contain relative z-10"
         />
+        <div className="flex sm:gap-6 gap-4 absolute-bottom-[5%] sm:left-[10%] max-sm:px-6">
+          {shoes.map((shoe) => (
+            <div key={shoe}>
+              <ShoeCard
+                ingURL={shoe}
+                changeBigShoeImage={() => {}}
+                bigShoeImg=""
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
