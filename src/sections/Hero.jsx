90745463d448923c6daafb5ae7903b9ebe5030f4
halaluddin2000 <helaluddin7001@gmail.com>
2024-12-27 23:21:58 +0600

    hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,14 +3,16 @@ import { arrowRight } from "../assets/icons";
 import { shoes, statistics } from "../constants";
 import { bigShoe1 } from "../assets/images";
 import ShoeCard from "../Components/ShoeCard";
+import { useState } from "react";
 
 const Hero = () => {
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
   return (
     <section
       id="home"
       className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
-      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28 padding-x">
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
         <p className="text-xl font-montserrat text-coral-red mt-14">
           Our Summer Collection
         </p>
@@ -40,19 +42,21 @@ const Hero = () => {
       {/* ------hero img-------- */}
       <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
         <img
-          src={bigShoe1}
+          src={bigShoeImg}
           alt="shoe collection"
           width={610}
           height={500}
           className="object-contain relative z-10"
         />
-        <div className="flex sm:gap-6 gap-4 absolute-bottom-[5%] sm:left-[10%] max-sm:px-6">
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
           {shoes.map((shoe) => (
             <div key={shoe}>
               <ShoeCard
-                ingURL={shoe}
-                changeBigShoeImage={() => {}}
-                bigShoeImg=""
+                imgURL={shoe}
+                changeBigShoeImage={(shoe) => {
+                  setBigShoeImg(shoe);
+                }}
+                bigShoeImg={bigShoeImg}
               />
             </div>
           ))}
