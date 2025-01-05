a04046205f0b505f0e755d3aca3bd442fd35367a
halaluddin2000 <helaluddin7001@gmail.com>
2025-01-06 03:14:59 +0600

    subscribe section added

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,5 +1,23 @@
-function Subscribe() {
-  return <div>Subscribe</div>;
-}
+import Button from "../constants/Button";
+
+const Subscribe = () => {
+  return (
+    <section
+      id="contact-us"
+      className="max-container flex justify-between items-center max-lg:flex-col gap-10"
+    >
+      <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold">
+        Sign Up for
+        <span className="text-coral-red"> Updates </span>& Newsletter
+      </h3>
+      <div className="lg:max-w-[40%] w-full flex items-center gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
+        <input type="text" placeholder="subscribe@nike.com" className="input" />
+        <div className="flex -ml-40 items-center ">
+          <Button label="Sign Up" fullWidth />
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Subscribe;
